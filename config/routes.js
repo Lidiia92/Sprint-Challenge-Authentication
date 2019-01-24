b04b10ff29a915915126e457b64638206262e84d
Lidii <lidiiany92@gmail.com>
2019-01-24 17:05:23 -0500

    Getting jokes from api/jokes

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,7 @@ const db = require('../database/dbConfig.js');
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.get('/api/jokes', helpers.protected, getJokes);
   //Test
   server.get('/', (req, res) => {
     res.send('Testing testing 1 2 3');
@@ -31,17 +31,54 @@ async function register (req, res) {
   }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  try {
+    const userCredentials = req.body;
+    const userForCheck = await db("users")
+    .where({ username: userCredentials.username })
+    .first();
+
+    if (
+    userForCheck &&
+    bcrypt.compareSync(userCredentials.password, userForCheck.password)
+    ) {
+    // login is successful
+    // create the token
+    const token = helpers.generateToken(userForCheck);
+
+    res
+        .status(200)
+        .json({ message: `Welcome ${userForCheck.username}`, token });
+    } else {
+    res.status(401).json({
+        message:
+        "Your login attempt failed. Please check login and password and try again"
+    });
+    }
+  } catch (err) {
+      res.status(500).json({ message: "Your login attempt failed." });
+  }
 }
 
-function getJokes(req, res) {
-  axios
-    .get('https://safe-falls-22549.herokuapp.com/random_ten')
-    .then(response => {
-      res.status(200).json(response.data);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+async function getJokes(req, res) {
+  try {
+    const endpoint = 'http://api.icndb.com/jokes/random/';
+    const response = await axios.get(endpoint);
+    const { data } = await response;
+    console.log(data);
+    res.status(200).json(response.data);
+  }
+  catch (err){
+    res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+  }
 }
+
+// axios
+// .get('https://safe-falls-22549.herokuapp.com/random_ten')
+// .then(response => {
+//   res.status(200).json(response.data);
+// })
+// .catch(err => {
+//   res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+// });
